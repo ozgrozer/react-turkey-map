0fe07bab194312612397c2ea8565f939dd5142d7
mac <ozgr@live.com>
2024-05-31 01:57:16 +0300

    Show cities

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,34 @@
 import { createRoot } from 'react-dom/client'
 
+import cities from './cities'
+
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <svg
+        version='1.1'
+        id='svg-turkiye-haritasi'
+        viewBox='0 0 1007.478 527.323'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g id='turkiye'>
+          {
+            cities.map((city, key) => {
+              return (
+                <g
+                  key={key}
+                  id={city.plate}
+                  data-name={city.name}
+                  data-plate={city.plate}
+                >
+                  <path d={city.draw} />
+                </g>
+              )
+            })
+          }
+        </g>
+      </svg>
+    </div>
   )
 }
 
