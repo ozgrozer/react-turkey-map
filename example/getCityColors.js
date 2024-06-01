37b2fab16a2d4f0ab51e9ee1fa0d547df5c4724d
mac <ozgr@live.com>
2024-06-01 16:09:40 +0300

    Update colors

--- example/getCityColors.js
+++ example/getCityColors.js
@@ -1,11 +1,11 @@
 const colorBands = [
-  { threshold: 10000000, color: '#071E58' }, // Over 10.000.000
-  { threshold: 3000000, color: '#253494' }, // 3.000.000 - 10.000.000
-  { threshold: 2000000, color: '#225EA8' }, // 2.000.000 - 3.000.000
-  { threshold: 1000000, color: '#1E91C0' }, // 1.000.000 - 2.000.000
-  { threshold: 500000, color: '#30A4C2' }, // 500.000 - 1.000.000
-  { threshold: 250000, color: '#41B6C4' }, // 250.000 - 500.000
-  { threshold: 0, color: '#85cfd8' } // Under 250.000
+  { threshold: 10000000, color: '#0B3D91' }, // Over 10.000.000
+  { threshold: 3000000, color: '#1E5AA6' }, // 3.000.000 - 10.000.000
+  { threshold: 2000000, color: '#2978B5' }, // 2.000.000 - 3.000.000
+  { threshold: 1000000, color: '#3490C7' }, // 1.000.000 - 2.000.000
+  { threshold: 500000, color: '#52A1CF' }, // 500.000 - 1.000.000
+  { threshold: 250000, color: '#73B9D9' }, // 250.000 - 500.000
+  { threshold: 0, color: '#85CFD8' } // Under 250.000
 ]
 
 const getColorForPopulation = population => {
