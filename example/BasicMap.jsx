f072cb34355d68a7328129fdcad2e21c42f4b95b
mac <ozgr@live.com>
2024-06-01 14:53:14 +0300

    Add basic map

---
+++ example/BasicMap.jsx
@@ -0,0 +1,12 @@
+// development
+import TurkeyMap from './../src/TurkeyMap'
+// build test
+// import TurkeyMap from './../build/TurkeyMap'
+// npm test
+// import TurkeyMap from 'react-turkey-map'
+
+export default () => {
+  return (
+    <TurkeyMap />
+  )
+}
--- example/index.jsx
+++ example/index.jsx
@@ -1,30 +1,12 @@
 import { createRoot } from 'react-dom/client'
 
-// development
-import TurkeyMap from './../src/TurkeyMap'
-
-// build test
-// import TurkeyMap from './../build/TurkeyMap'
-
-// npm test
-// import TurkeyMap from 'react-turkey-map'
+import BasicMap from './BasicMap'
 
 const App = () => {
-  const colorData = {
-    '07': 'red',
-    '06': 'green'
-  }
-
-  const tooltipData = {
-    '07': '150',
-    '06': '12'
-  }
-
   return (
-    <TurkeyMap
-      colorData={colorData}
-      tooltipData={tooltipData}
-    />
+    <div>
+      <BasicMap />
+    </div>
   )
 }
 
