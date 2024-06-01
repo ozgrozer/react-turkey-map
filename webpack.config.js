fc71b4e9ed39df9cd811e5860499b31aefd46c42
mac <ozgr@live.com>
2024-06-01 05:13:26 +0300

    Another approach for supporting both node and browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,42 @@
 const path = require('path')
 
-module.exports = {
+// Configuration for the browser
+const browserConfig = {
   mode: 'production',
+  target: 'web',
+  entry: './src/TurkeyMap.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'TurkeyMap.web.js',
+    library: 'TurkeyMap',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom'
+  }
+}
+
+// Configuration for Node.js
+const nodeConfig = {
+  mode: 'production',
+  target: 'node',
   entry: './src/TurkeyMap.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -30,3 +65,5 @@ module.exports = {
     'react-dom': 'commonjs react-dom'
   }
 }
+
+module.exports = [browserConfig, nodeConfig]
