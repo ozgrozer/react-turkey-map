785e74e4a2f6234e5115b2dbbc01b7833e13bc84
mac <ozgr@live.com>
2024-06-01 14:58:56 +0300

    Add example page styles

--- example/index.jsx
+++ example/index.jsx
@@ -1,10 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { createRoot } from 'react-dom/client'
 
 import BasicMap from './BasicMap'
 
+const styles = {
+  wrapper: css`
+    width: 90%;
+    margin: auto;
+    h1 {
+      padding: 10px 20px;
+      margin-bottom: 50px;
+      background-color: #eee;
+    }
+  `
+}
+
 const App = () => {
   return (
-    <div>
+    <div css={styles.wrapper}>
+      <h1>Basic Map</h1>
       <BasicMap />
     </div>
   )
